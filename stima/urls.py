7489eaa9c30892ecdf9fd0675c792d4b3c770292
iPolarisu <elpolarisu@gmail.com>
2022-05-18 14:03:09 -0400

    deleted cash and tarro for re-written version of stimame

--- stima/urls.py
+++ stima/urls.py
@@ -4,7 +4,5 @@ from . import views
 urlpatterns = [
         path('', views.index, name='index'),
         path('stima-score/', views.stimaScore, name='stima-score'),
-        path('stima-tarro/', views.stimaTarro, name='stima-tarro'),
-        path('stima-cash/', views.stimaCash, name='stima-cash'),
         path('game/<int:id>', views.game, name='game')
         ]
