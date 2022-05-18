1de151f62a4df3e600e799a32bad4d3563d2ab7d
iPolarisu <elpolarisu@gmail.com>
2022-05-18 01:31:08 -0400

    fixed comma missing

--- stima/urls.py
+++ stima/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
         path('', views.index, name='index'),
         path('stima-score/', views.stimaScore, name='stima-score'),
         path('stima-tarro/', views.stimaTarro, name='stima-tarro'),
-        path('stima-cash/', views.stimaCash, name='stima-cash')
+        path('stima-cash/', views.stimaCash, name='stima-cash'),
         path('game/<int:id>', views.game, name='game')
         ]
